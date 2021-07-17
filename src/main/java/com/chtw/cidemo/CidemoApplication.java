67e822889d82b7ed5a7f1c4815fece7427c06ea6
陈毫 <chenhao03@xiaoduotech.com>
2021-07-17 23:20:58 +0800

    init

---
+++ src/main/java/com/chtw/cidemo/CidemoApplication.java
@@ -0,0 +1,13 @@
+package com.chtw.cidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CidemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/chtw/cidemo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.chtw.cidemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/state")
+    public Map<String, String> test(){
+        Map<String, String> res = new HashMap<>();
+        res.put("code", "0");
+        res.put("msg", "server is ok");
+        return res;
+    }
+}
